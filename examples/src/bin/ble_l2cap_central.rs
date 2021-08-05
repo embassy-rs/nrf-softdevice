29211a57b49b7c7b80a54ccd35336adff001f6b2
F_Punk <fabiopungg@gmail.com>
2021-08-05 19:00:02 +0200

    update embassy and toolchain (#76)
    * update toolchain to nightly-08-03
    update embassy
    remove removed features
    
    * oops, missed a toolchain file

--- examples/src/bin/ble_l2cap_central.rs
+++ examples/src/bin/ble_l2cap_central.rs
@@ -1,8 +1,6 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
-#![feature(min_type_alias_impl_trait)]
-#![feature(impl_trait_in_bindings)]
 #![feature(alloc_error_handler)]
 #![allow(incomplete_features)]
 extern crate alloc;
