f5d1a270e32c54c6f5963be3a7ce482a601914c9
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-05-12 23:00:48 +0200

    Remove unused/unneeded features.

--- nrf-softdevice/src/lib.rs
+++ nrf-softdevice/src/lib.rs
@@ -1,9 +1,6 @@
 #![no_std]
 #![feature(generic_associated_types)]
 #![feature(type_alias_impl_trait)]
-#![feature(const_fn_trait_bound)]
-#![feature(cell_update)]
-#![allow(incomplete_features)]
 
 pub(crate) mod util;
 
