49fb40659d646b9d3aa9d174a030ca67c043bbaf
alexmoon <alex.r.moon@gmail.com>
2022-09-14 17:22:45 -0400

    Add support for remaining GATT server events

--- nrf-softdevice/src/ble/replies.rs
+++ nrf-softdevice/src/ble/replies.rs
@@ -142,3 +142,104 @@ impl SysAttrsReply {
         res
     }
 }
+
+#[cfg(feature = "ble-gatt-server")]
+const DEFERRED_TYPE_READ: u8 = raw::BLE_GATTS_AUTHORIZE_TYPE_READ as u8;
+#[cfg(feature = "ble-gatt-server")]
+const DEFERRED_TYPE_WRITE: u8 = raw::BLE_GATTS_AUTHORIZE_TYPE_WRITE as u8;
+
+#[cfg(feature = "ble-gatt-server")]
+struct DeferredReply<const DEFERRED_TYPE: u8> {
+    conn: ManuallyDrop<Connection>,
+}
+
+#[cfg(feature = "ble-gatt-server")]
+impl<const DEFERRED_TYPE: u8> Drop for DeferredReply<DEFERRED_TYPE> {
+    fn drop(&mut self) {
+        warn!("DeferredReply<{}> dropped without reply", DEFERRED_TYPE);
+        let res = unsafe { self.finalize(raw::BLE_GATT_STATUS_ATTERR_UNLIKELY_ERROR as u16, &[], false) };
+
+        if let Err(_err) = res {
+            warn!("sd_ble_gatts_rw_authorize_reply err {:?}", _err);
+        }
+    }
+}
+
+#[cfg(feature = "ble-gatt-server")]
+impl<const DEFERRED_TYPE: u8> DeferredReply<DEFERRED_TYPE> {
+    fn reply(mut self, status: u16, data: &[u8], update: bool) -> Result<(), RawError> {
+        let res = unsafe { self.finalize(status, data.as_ref(), update) };
+        core::mem::forget(self);
+        res
+    }
+
+    /// # Safety
+    ///
+    /// This method must be called exactly once
+    unsafe fn finalize(&mut self, status: u16, data: &[u8], update: bool) -> Result<(), RawError> {
+        let res = if let Some(handle) = self.conn.handle() {
+            let params = raw::ble_gatts_authorize_params_t {
+                gatt_status: status,
+                _bitfield_1: raw::ble_gatts_authorize_params_t::new_bitfield_1(u8::from(update)),
+                offset: 0,
+                len: data.len() as u16,
+                p_data: data.as_ptr(),
+            };
+
+            let reply_params = raw::ble_gatts_rw_authorize_reply_params_t {
+                type_: DEFERRED_TYPE,
+                params: raw::ble_gatts_rw_authorize_reply_params_t__bindgen_ty_1 { read: params },
+            };
+
+            let ret = raw::sd_ble_gatts_rw_authorize_reply(handle, &reply_params);
+            RawError::convert(ret)
+        } else {
+            Err(RawError::BleInvalidConnHandle)
+        };
+
+        // Since conn is ManuallyDrop, we must drop it here
+        ManuallyDrop::drop(&mut self.conn);
+        res
+    }
+}
+
+#[cfg(feature = "ble-gatt-server")]
+pub struct DeferredWriteReply(DeferredReply<DEFERRED_TYPE_WRITE>);
+
+#[cfg(feature = "ble-gatt-server")]
+impl DeferredWriteReply {
+    pub(crate) fn new(conn: Connection) -> Self {
+        DeferredWriteReply(DeferredReply {
+            conn: ManuallyDrop::new(conn),
+        })
+    }
+
+    pub fn conn(&self) -> &Connection {
+        &self.0.conn
+    }
+
+    pub fn reply<T: AsRef<[u8]>>(self, status: u16, data: &T) -> Result<(), RawError> {
+        let update = u32::from(status) == raw::BLE_GATT_STATUS_SUCCESS;
+        self.0.reply(status, data.as_ref(), update)
+    }
+}
+
+#[cfg(feature = "ble-gatt-server")]
+pub struct DeferredReadReply(DeferredReply<DEFERRED_TYPE_READ>);
+
+#[cfg(feature = "ble-gatt-server")]
+impl DeferredReadReply {
+    pub(crate) fn new(conn: Connection) -> Self {
+        DeferredReadReply(DeferredReply {
+            conn: ManuallyDrop::new(conn),
+        })
+    }
+
+    pub fn conn(&self) -> &Connection {
+        &self.0.conn
+    }
+
+    pub fn reply<T: AsRef<[u8]>>(self, status: u16, data: &T, update: bool) -> Result<(), RawError> {
+        self.0.reply(status, data.as_ref(), update)
+    }
+}
