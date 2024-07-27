0122485d9f71f67f9471cf3323ef7f4b946bf834
Bart Massey <bart.massey@gmail.com>
2024-07-27 13:51:44 -0700

    updated examples to work with BBC micro:bit v2

--- examples/src/bin/ble_peripheral_onoff.rs
+++ examples/src/bin/ble_peripheral_onoff.rs
@@ -121,10 +121,17 @@ async fn main(spawner: Spawner) {
     unwrap!(spawner.spawn(softdevice_task(sd)));
 
     info!("Bluetooth is OFF");
-    info!("Press nrf52840-dk button 1 to enable, button 2 to disable");
+    info!("Press button 1 to enable, button 2 to disable");
+    #[cfg(feature = "microbit-v2")]
+    let (b1_pin, b2_pin) = (p.P0_14, p.P0_23);
+    #[cfg(feature = "nrf52840-dk")]
+    let (b1_pin, b2_pin) = (p.P0_11, p.P0_12);
+    #[cfg(not(any(feature = "nrf52840-dk", feature = "microbit-v2")))]
+    panic!("unsupported board");
 
-    let button1 = Input::new(p.P0_11.degrade(), Pull::Up);
-    let button2 = Input::new(p.P0_12.degrade(), Pull::Up);
+    let button1 = Input::new(b1_pin.degrade(), Pull::Up);
+    let button2 = Input::new(b2_pin.degrade(), Pull::Up);
+
     pin_mut!(button1);
     pin_mut!(button2);
     loop {
